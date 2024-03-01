9ddef52dd019e619237deb90f18918449254843d
Anshul Verma <anshul.verma@berkeley.edu>
2024-03-01 15:17:20 -0800

    Broken websockets import

--- zetamac-frontend/src/pages/MainPage.jsx
+++ zetamac-frontend/src/pages/MainPage.jsx
@@ -4,8 +4,12 @@ import { Button } from '@/components/ui/button';
 import CreateRoomButton from '@/components/ui/CreateRoomButton';
 import { User } from 'lucide-react';
 import RoomGallery from '@/components/ui/RoomGallery';
+import { useUser } from '@clerk/clerk-react';
   
 function MainPage() {
+    const { isLoaded, user } = useUser();
+    console.log("HERE!")
+    console.log(user);
     return (
         <>
         <div>
@@ -17,7 +21,7 @@ function MainPage() {
                     Join or create a room to start playing!
         </h1>
         <RoomGallery/>
-        <CreateRoomButton/>
+        <CreateRoomButton user={user}/>
         </div>
         
         </>
